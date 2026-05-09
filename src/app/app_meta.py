46b50c842433726dd7ca8da51529e9793ba0b6cc
liuvgg <2627538472@qq.com>
2026-05-10 01:23:22 +0800

    补充说明

--- src/app/app_meta.py
+++ src/app/app_meta.py
@@ -1,5 +1,11 @@
 APP_META = {
-
+    "version": "1.0.75",
+    "notice_url": "https://videoseek-models.oss-cn-hangzhou.aliyuncs.com/notice.json",
+    "about_url": "https://videoseek-models.oss-cn-hangzhou.aliyuncs.com/about.json",
+    "version_url": "https://videoseek-models.oss-cn-hangzhou.aliyuncs.com/version.json",
+    "model_manifest_url": "https://1858268090.share.123pan.cn/123pan/VFA7vd-vhJXA",
+    "remote_index_manifest_url": "",
+    "remote_timeout": 4,
 }
 
 
